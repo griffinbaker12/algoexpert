e3df1421751ffa01223ab0954d2a187b99e99399
griffinbaker12 <griffin_baker12@icloud.com>
2022-07-03 22:42:10 -0500

    palindrome and selection sort, final for the day

---
+++ easy/isPalindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(string) {
+  // Write your code here.
+  let p1 = 0;
+  let p2 = string.length - 1;
+  while (p1 < p2) {
+    if (string[p1] !== string[p2]) return false;
+    p1++;
+    p2--;
+  }
+  return true;
+}
