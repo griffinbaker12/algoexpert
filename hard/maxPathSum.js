9f3db10d08b99501f5d0e20dfbe40696f923b9d8
griffinbaker12 <griffin_baker12@icloud.com>
2022-07-28 22:29:06 -0500

    changes

--- hard/maxPathSum.js
+++ hard/maxPathSum.js
@@ -5,14 +5,17 @@ function maxPathSum(tree) {
 
 function helper(tree) {
   if (!tree) return [0, -Infinity];
-  const [leftOne, leftTwo] = helper(tree.left);
-  const [rightOne, rightTwo] = helper(tree.right);
+  const [leftMaxAsAsBranch, leftMaxPath] = helper(tree.left);
+  const [rightMaxAsBranch, rightMaxPath] = helper(tree.right);
   const maxAndCurr = Math.max(
     tree.value,
-    tree.value + Math.max(leftOne, rightOne)
+    tree.value + Math.max(leftMaxAsAsBranch, rightMaxAsBranch)
   );
-  const throughAsRoot = Math.max(maxAndCurr, tree.value + leftOne + rightOne);
-  const maxPathSum = Math.max(leftTwo, rightTwo, throughAsRoot);
+  const throughAsRoot = Math.max(
+    maxAndCurr,
+    tree.value + leftMaxAsAsBranch + rightMaxAsBranch
+  );
+  const maxPathSum = Math.max(leftMaxPath, rightMaxPath, throughAsRoot);
   return [maxAndCurr, maxPathSum];
 }
 
